bc49879dddb271f5e6b6a98f2264bd3637c0d5a1
idl99 <ihandilnath2012@gmail.com>
2019-04-07 17:44:47 +0530

    extended ProductFormActivity to support editing a product

--- app/src/main/java/com/ihandilnath/mykitchenapp/ui/ProductFormActivity.java
+++ app/src/main/java/com/ihandilnath/mykitchenapp/ui/ProductFormActivity.java
@@ -2,8 +2,10 @@ package com.ihandilnath.mykitchenapp.ui;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.ihandilnath.mykitchenapp.R;
 import com.ihandilnath.mykitchenapp.databinding.ActivityProductFormBinding;
+import com.ihandilnath.mykitchenapp.db.Product;
 import com.ihandilnath.mykitchenapp.viewmodel.ProductFormViewModel;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,13 +20,21 @@ public class ProductFormActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_product_form);
 
         if(mViewModel == null){
             mViewModel = ViewModelProviders.of(this).get(ProductFormViewModel.class);
         }
 
-        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_product_form);
+        if(getIntent().getExtras().containsKey("product")){
+            mViewModel.setProduct((Product) getIntent().getExtras().get("product"));
+            TextInputEditText tv = findViewById(R.id.productform_edit_name);
+            tv.setFocusable(false);
+        }
+
         mDataBinding.setViewmodel(mViewModel);
+
+
     }
 
 }
